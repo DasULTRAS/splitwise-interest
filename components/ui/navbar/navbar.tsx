e440f87afb4ed59fd21d243c1877f4d62b0fbb64
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2023-11-09 19:55:29 +0100

    15 new login button design (#16)
    * EDIT: Avatar handling
    
    * EDIT: Login button design and reduced to big fonts

--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -4,28 +4,36 @@ import { getServerSession } from "next-auth/next";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import Favicon from "@/components/ui/images/favicon";
 import Link from "next/link";
+import SidebarTrigger from "../sidebar/sidebarTigger";
+import LoginButton from "../buttons/loginButton";
 
 export default async function Navbar() {
     const session: Session | null = await getServerSession(options);
 
     return (
-        <div className="sticky row-span-3 flex w-screen bg-black/20 px-4 py-3 dark:bg-black/30">
-            <div className="w-1/5">
+        <div className="sticky w-screen h-16 p-3 row-span-3 flex bg-black/30 shadow-md shadow-black/30">
+            <div className="w-16">
                 <a title="home" href="/">
-                    <Favicon width={48} height={48}/>
+                    <Favicon width={40} height={40} className="w-10 h-10" />
                 </a>
             </div>
 
-            <nav className="flex w-4/6 items-center">
-                <ul className="flex w-full justify-between space-x-2">
-                    <li className="btn_clickable rounded-2xl p-2"><Link href="/">Home</Link></li>
-                    <li className="btn_clickable rounded-2xl p-2"><Link href="/dashboard">Dashboard</Link></li>
-                    <li className="btn_clickable rounded-2xl p-2"><Link href="/action">Action</Link></li>
-                </ul>
-            </nav>
+            {session &&
+                <nav className="flex w-4/6 items-center">
+                    <ul className="flex w-full justify-start space-x-2">
+                        <li className="btn_clickable rounded-2xl p-2"><Link href="/dashboard">Dashboard</Link></li>
+                        <li className="btn_clickable rounded-2xl p-2"><Link href="/action">Action</Link></li>
+                    </ul>
+                </nav>
+            }
 
-            <div className="ml-auto flex w-1/5 flex-row-reverse">
-                <UserAvatar session={session}/>
+            <div className="ml-auto w-1/6 flex flex-row-reverse">
+                {session ?
+                    <SidebarTrigger session={session}>
+                        <UserAvatar session={session} height={40} width={40} className="w-10 h-10" />
+                    </SidebarTrigger>
+                    : <LoginButton />
+                }
             </div>
         </div>
     );
--- components/ui/sidebar/sidebar.tsx
+++ components/ui/sidebar/sidebar.tsx
@@ -1,21 +1,19 @@
-import {DefaultUser, Session} from "next-auth";
+import { DefaultUser, Session } from "next-auth";
 import LoginButton from '../buttons/loginButton';
 import React from "react";
 import Link from "next/link";
 
 interface SidebarIsOpenProps {
-    sidebarIsOpen: boolean;
     closeModal: () => void;
     session?: Session | null;
 }
 
-export default function Sidebar({sidebarIsOpen, closeModal, session}: SidebarIsOpenProps) {
+export default function Sidebar({ closeModal, session }: SidebarIsOpenProps) {
     return (
         <>
-            {
-                sidebarIsOpen &&
-                <div className="absolute top-0 left-0 z-10 h-screen w-screen bg-white/10"
-                     onClick={closeModal}>
+            <div className="absolute top-0 left-0 z-10 h-screen w-screen bg-white/10">
+                <div className="flex">
+                    <div className="w-full h-screen" onClick={closeModal} />
                     <div
                         className="top-0 ml-auto flex h-screen flex-col rounded-2xl bg-black/80 p-6 w-3/4 sm:w-2/4 md:w-1/4">
                         <div className="flex">
@@ -24,25 +22,26 @@ export default function Sidebar({sidebarIsOpen, closeModal, session}: SidebarIsO
                             <button title="btn_sidebar_close" className="ml-auto text-white" onClick={closeModal}>
                                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M6 18L18 6M6 6l12 12"></path>
+                                        d="M6 18L18 6M6 6l12 12"></path>
                                 </svg>
                             </button>
                         </div>
 
-                        <hr className="mb-6 border-t"/>
+                        <hr className="mb-6 border-t" />
 
-                        <ul className="space-y-2 text-white">
+                        <ul className="space-y-2 text-white"
+                            onClick={closeModal}>
                             <li>
                                 <Link href="/settings" className="btn_clickable p-2 rounded-2xl">Settings</Link>
                             </li>
                         </ul>
 
                         <div className="flex mt-auto ml-auto text-white items-center">
-                            <LoginButton user={session?.user as DefaultUser}/>
+                            <LoginButton user={session?.user as DefaultUser} />
                         </div>
                     </div>
                 </div>
-            }
+            </div>
         </>
     );
 }
