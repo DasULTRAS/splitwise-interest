e11f9eeb269ee7ca8f2d564d0fe71bbfd3e6ea12
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2023-11-12 15:08:16 +0100

    18 incorrect feedback when splitwise credentials are incorrect (#20)
    * EDIT: Splitwise settings form layout
    
    * EDIT: use NextJS Function useParams to get Page param
    
    * ADD: error handling for unauthorized access and
    Splitwise connection
    
    * EDIT: one statement for unknown error

--- components/ui/unauthorised/page.tsx
+++ components/ui/unauthorised/page.tsx
@@ -1,8 +1,8 @@
-export default function UnauthorizedPage({ href, anchorText }: { href?: string, anchorText?: string }) {
+export default function UnauthorizedPage({ children, href, anchorText }: { children?: React.ReactNode, href?: string, anchorText?: string }) {
     return (
         <div className="flex h-full w-full flex-col justify-center px-5 text-center">
             <h1 className="mb-2 text-2xl font-bold">You are unauthorized to see this Page.</h1>
-            <a className="px-10" href={href || "/"}>{anchorText || "Go back to Home Page"}</a>
+            <a className="px-10" href={href || "/"}>{children || anchorText || "Go back to Home Page"}</a>
         </div>
     );
 }
