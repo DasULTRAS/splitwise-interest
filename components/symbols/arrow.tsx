157756549bad09dc474a419ad3556180a87eb405
DasULTRAS <christian@hoelter.ch>
2023-11-12 23:37:56 +0100

    FIX: stroke attributes in arrow svg symbols

--- components/symbols/arrow.tsx
+++ components/symbols/arrow.tsx
@@ -2,7 +2,7 @@ export function ArrowHeadUp({ width = "24", height = "24" }: { width?: string, h
     return (
         <>
             <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 15l-6-6-6 6" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 15l-6-6-6 6" />
             </svg>
         </>
     );
@@ -12,7 +12,7 @@ export function ArrowHeadDown({ width = "24", height = "24" }: { width?: string,
     return (
         <>
             <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 9l6 6 6-6" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 9l6 6 6-6" />
             </svg>
         </>
     );
