ee874e5bdb4784128e0d98939960093416179628
DasULTRAS <christian@hoelter.ch>
2024-08-09 23:07:12 +0200

    EDIT: Devcontainer optimiert EDIT: node_modules gecached in eigenem Volume

--- db/mongo-init.js
+++
@@ -1,9 +0,0 @@
-const appDb = db.getSiblingDB('app');
-//appDb.createCollection('users');
-appDb.users.insertOne({
-    username: "root",
-    password: "$2b$10$As3m.mkxjLiwQc4GTOcteu/65UxlXp.MnCQlKYthzFszGy19Kc3Tq",
-    email: "root@change.me"
-});
-// default user root with password changeme
-console.log('mongo-init.js: init root user!');
