6cddee5acb201ca627d1884315d0bf737ca70f0c
DasULTRAS <christian@hoelter.ch>
2024-08-11 19:09:00 +0200

    ADD: event console logs ADD: callback to redirect relative

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,30 @@
+import Splitwise from "@/utils/splitwise/splitwise";
 import NextAuth from "next-auth";
 import { options } from "./auth.config";
 
-const handler = NextAuth(options);
+const handler = NextAuth({
+  events: {
+    async signIn(message) {
+      console.log(`User ${message?.user?.name} logged in with ${message.account?.type}.`);
+    },
+    async signOut(message) {
+      console.log(`User ${message?.token?.name} logged out.`);
+
+      if (message.token.name) await Splitwise.resetInstanceByUsername(message.token.name);
+    },
+    async createUser(message) {
+      console.log("User created: ", message);
+    },
+    async session(message) {
+      if (options.debug) console.log("Session: ", message);
+    },
+  },
+  callbacks: {
+    async redirect({ url }) {
+      return Promise.resolve(url);
+    },
+  },
+  ...options,
+});
 
 export { handler as GET, handler as POST };
